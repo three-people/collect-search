207a3568cff0e95e3bec3ef91e1feb9bca47f251
zhimingyang <815138196@qq.com>
2016-11-28 00:20:16 +0800

    更新es脚本

--- doc/shell/esmapping.sh
+++ doc/shell/esmapping.sh
@@ -2,8 +2,8 @@
 #生成es相关的person索引
 escluster_host=$1
 
-curl -XPUT http://192.168.0.107:9200/people_index
-curl -XPOST http://192.168.0.107:9200/people_index/fulltext/_mapping -d'
+curl -XPUT http://127.0.0.1:9200/people_index
+curl -XPOST http://127.0.0.1:9200/people_index/fulltext/_mapping -d'
 {
     "fulltext": {
         "_all": {
@@ -58,8 +58,8 @@ curl -XPOST http://192.168.0.107:9200/people_index/fulltext/_mapping -d'
 
 
 #房屋相关
-curl -XPUT http://192.168.0.107:9200/house_index
-curl -XPOST http://192.168.0.107:9200/house_index/fulltext/_mapping -d'
+curl -XPUT http://127.0.0.1:9200/house_index
+curl -XPOST http://127.0.0.1:9200/house_index/fulltext/_mapping -d'
 {
     "fulltext": {
         "_all": {
@@ -118,8 +118,8 @@ curl -XPOST http://192.168.0.107:9200/house_index/fulltext/_mapping -d'
     }
 }'
 #雇佣关系相关索引结构
-curl -XPUT http://192.168.0.107:9200/employer_index
-curl -XPOST http://192.168.0.107:9200/employer_index/fulltext/_mapping -d'
+curl -XPUT http://127.0.0.1:9200/employer_index
+curl -XPOST http://127.0.0.1:9200/employer_index/fulltext/_mapping -d'
 {
     "fulltext": {
         "_all": {
@@ -172,8 +172,8 @@ curl -XPOST http://192.168.0.107:9200/employer_index/fulltext/_mapping -d'
     }
 }'
 #场所
-curl -XPUT http://192.168.0.107:9200/place_index
-curl -XPOST http://192.168.0.107:9200/place_index/fulltext/_mapping -d'
+curl -XPUT http://127.0.0.1:9200/place_index
+curl -XPOST http://127.0.0.1:9200/place_index/fulltext/_mapping -d'
 {
     "fulltext": {
         "_all": {
@@ -230,8 +230,8 @@ curl -XPOST http://192.168.0.107:9200/place_index/fulltext/_mapping -d'
 }'
 
 #监控相关
-curl -XPUT http://192.168.0.107:9200/camera_index
-curl -XPOST http://192.168.0.107:9200/camera_index/fulltext/_mapping -d'
+curl -XPUT http://127.0.0.1:9200/camera_index
+curl -XPOST http://127.0.0.1:9200/camera_index/fulltext/_mapping -d'
 {
     "fulltext": {
         "_all": {
